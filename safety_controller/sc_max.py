a74f7116fe52b5fa689a509eb7a256f49f140695
maxzetina <74638275+maxzetina@users.noreply.github.com>
2024-03-08 18:54:40 -0500

    vel to 3.5

--- safety_controller/sc_max.py
+++ safety_controller/sc_max.py
@@ -46,7 +46,7 @@ class SafetyController(Node):
         
         self.stop_distance = 0.4 # m
         self.stopping = False
-        self.VELOCITY = 1.0
+        self.VELOCITY = 3.5
         self.a = None
         self.get_logger().info('HERE "%s"' % self.SAFETY_TOPIC)
 
