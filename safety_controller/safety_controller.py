13640975ac965e109bcd80d425b7d4f693ef1812
maxzetina <74638275+maxzetina@users.noreply.github.com>
2024-03-10 18:08:30 -0400

    launch file for robot

--- safety_controller/safety_controller.py
+++ safety_controller/safety_controller.py
@@ -25,25 +25,17 @@ class SafetyController(Node):
 
         # Declare parameters to make them available for use
         self.declare_parameter("scan_topic", "default")
-        self.declare_parameter("sim_safety_topic", "default")
-        self.declare_parameter("sim_navigation_topic", "default")
+        self.declare_parameter("safety_topic", "default")
+        self.declare_parameter("navigation_topic", "default")
         self.declare_parameter("stop_range", "default")
 
         # Fetch constants from the ROS parameter server
         self.SCAN_TOPIC = self.get_parameter('scan_topic').get_parameter_value().string_value
-<<<<<<< HEAD
         self.SAFETY_TOPIC = self.get_parameter('safety_topic').get_parameter_value().string_value
         self.NAVIGATION_TOPIC = self.get_parameter('navigation_topic').get_parameter_value().string_value
         # self.STOP_RANGE = self.get_parameter("stop_range").get_parameter_value().double_value
 
         self.sub_navigation = self.create_subscription(AckermannDriveStamped, self.NAVIGATION_TOPIC, self.navigation_callback, 10)
-=======
-        self.SAFETY_TOPIC = self.get_parameter('sim_safety_topic').get_parameter_value().string_value
-        self.NAVIGATION_TOPIC = self.get_parameter('sim_navigation_topic').get_parameter_value().string_value
-        self.STOP_RANGE = self.get_parameter("stop_range").get_parameter_value().double_value
-
-        self.sub_navigation = self.create_subscription(AckermannDriveStamped, self.NAVIGATION_TOPIC, self.navigation_callback, 10)
->>>>>>> 683857d0e986a80371fe9b657c3032c74d2e1570
         self.sub_scan = self.create_subscription(LaserScan, self.SCAN_TOPIC, self.scan_callback, 10)
         self.pub_safety = self.create_publisher(AckermannDriveStamped, "drive", 10)
 
@@ -95,7 +87,7 @@ class SafetyController(Node):
             stop_cmd.drive.speed = 0.0
             stop_cmd.drive.steering_angle = 0.0
         else:
-            stop_cmd.drive.speed = 5.0
+            stop_cmd.drive.speed = 1.0
             stop_cmd.drive.steering_angle = 0.0
         self.get_logger().info('stop_range: "%s"' % self.STOP_RANGE)
         stop_cmd.drive.steering_angle_velocity = 0.0
