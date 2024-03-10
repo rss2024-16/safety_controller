311af19648846d3c672f8c7166b6d0aec448b427
maxzetina <74638275+maxzetina@users.noreply.github.com>
2024-03-10 18:53:00 -0400

    new range

--- safety_controller/safety_controller.py
+++ safety_controller/safety_controller.py
@@ -80,7 +80,7 @@ class SafetyController(Node):
         '''
         distances, thetas = self.slice_ranges(laser_scan)
         stop_cmd = AckermannDriveStamped()
-        self.STOP_RANGE = ((3.0**2.0)/ (2.0*9.81)) + 0.1
+        self.STOP_RANGE = ((3.0**2.0)/ (2.0*9.81)) + 0.5
         if min(distances) < self.STOP_RANGE:
             # Example threshold, adjust as needed
             stop_cmd.drive.speed = 0.0
